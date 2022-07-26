e82a5003478005bbe05a0ce8ce5a5b531fc9e3e8
chenpeng <peng.chen@tenclass.com>
2022-07-26 11:49:33 +0800

    fix: Modify the analysis conditions

--- src/bot/bot.guards.ts
+++ src/bot/bot.guards.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { BotContext, GreetEvent } from './bot.interface';
 
 @Injectable()
 export class BotGuards {
-  private isDefaultScene(context: BotContext, event: GreetEvent): boolean {
-    return event.scene === 'default';
-  }
-
   public getGuards() {
-    return {
-      isDefaultScene: this.isDefaultScene.bind(this),
-    };
+    return {};
   }
 }
